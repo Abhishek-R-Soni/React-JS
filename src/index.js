812e100e6fa74f1a199228ce918ef7ac3a6e491c
Abhishek-R-Soni <abhisheksoni4195@gmail.com>
2020-07-27 15:35:27 +0530

    1st version

--- src/index.js
+++ src/index.js
@@ -1,15 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Service extends React.Component{
+  // return boolean value
+  isServiceAvailable(){
+    alert('Yes...')
+  }
+
+  // button
+  render(){
+    return <button onClick={this.isServiceAvailable}>Service</button>
+  }
+}
+
+// Car Component
+class Car extends React.Component{
+
+  // initialization state
+  constructor(){
+    super();
+    this.state = {
+      name: 'Car',
+      model: 'Tesla',
+      price: 12000
+    }
+  }
+
+  // render table
+  render(){
+    return (
+      <div>
+        <h1>Car Demo</h1>
+        <table>
+          <tr>
+            <th>Name</th>
+            <th>Model</th>
+            <th>Price</th>
+          </tr>
+
+          <tr>
+            <td>{this.state.name}</td>
+            <td>{this.state.model}</td>
+            <td>{this.state.price}</td>
+          </tr>
+          
+        </table>
+        
+        <Service />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<Car />, document.querySelector('#root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
